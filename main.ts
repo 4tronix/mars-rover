32c6eecd0cd8d0b861ec977e61cbdf292b15ed97
4tronix <Sales@4tronix.co.uk>
2019-09-14 14:26:10 +0100

    Added EEROM and LED Handlers

--- main.ts
+++ main.ts
@@ -47,6 +47,55 @@ enum eMotor
     Both
 }
 
+/**
+  * Stop modes. Coast or Brake
+  */
+enum eStopMode
+{
+    //% block="no brake"
+    Coast,
+    //% block="brake"
+    Brake
+}
+
+/**
+  * Update mode for LEDs
+  * setting to Manual requires show LED changes blocks
+  * setting to Auto will update the LEDs every time they change
+  */
+enum eUpdateMode
+{
+    Manual,
+    Auto
+}
+
+/**
+  * Pre-Defined LED colours
+  */
+enum eColors
+{
+    //% block=red
+    Red = 0xff0000,
+    //% block=orange
+    Orange = 0xffa500,
+    //% block=yellow
+    Yellow = 0xffff00,
+    //% block=green
+    Green = 0x00ff00,
+    //% block=blue
+    Blue = 0x0000ff,
+    //% block=indigo
+    Indigo = 0x4b0082,
+    //% block=violet
+    Violet = 0x8a2be2,
+    //% block=purple
+    Purple = 0xff00ff,
+    //% block=white
+    White = 0xffffff,
+    //% block=black
+    Black = 0x000000
+}
+
 
 /**
  * Custom blocks
@@ -62,21 +111,11 @@ namespace Rover
     let leftSpeed = 0;
     let rightSpeed = 0;
     let servoOffset: number[] = [];
+    let neoStrip: neopixel.Strip;
+    let _updateMode = eUpdateMode.Auto;
 
 
 // HELPER FUNCTIONS
-
-    /**
-      * Return servo number from name
-      *
-      * @param value servo name
-      */
-    //% blockId="cu_getServo" block="%value"
-    //% weight=80
-    export function getServo(value: eServos): number
-    {
-        return value;
-    }
 
     // initialise the servo driver and the offset array values
     function initPCA(): void
@@ -112,11 +151,13 @@ namespace Rover
             servoOffset[i] = readEEROM(i);
     }
 
-    function setPWM(): void
+    // slow PWM frequency for slower speeds to improve torque
+    // only one PWM frequency available for all pins
+    function setPWM(speed: number): void
     {
-        if ((leftSpeed < 400) || (rightSpeed < 400))
+        if (speed < 200)
             pins.analogSetPeriod(AnalogPin.P0, 60000);
-        else if ((leftSpeed < 600) || (rightSpeed < 600))
+        else if (speed < 300)
             pins.analogSetPeriod(AnalogPin.P0, 40000);
         else
             pins.analogSetPeriod(AnalogPin.P0, 30000);
@@ -128,8 +169,10 @@ namespace Rover
     /**
       * Initialise all servos to Angle=0
       */
-    //% blockId="cu_zeroServos"
+    //% blockId="zeroServos"
     //% block="Centre all servos"
+    //% weight=100
+    //% subcategory=Servos
     export function zeroServos(): void
     {
         for (let i=0; i<16; i++)
@@ -141,8 +184,10 @@ namespace Rover
       * @param servo Servo number (0 to 15)
       * @param angle degrees to turn servo (-90 to +90)
       */
-    //% blockId="cu_setServo" block="set servo %servo| to angle %angle"
-    //% weight = 70
+    //% blockId="setServo"
+    //% block="set servo %servo| to angle %angle"
+    //% weight=90
+    //% subcategory=Servos
     export function setServo(servo: number, angle: number): void
     {
         if (initI2C == false)
@@ -167,6 +212,34 @@ namespace Rover
         pins.i2cWriteBuffer(PCA, i2cData, false);
     }
 
+    /**
+      * Set Servo Offset then zero the servo
+      * @param servo Servo number (0 to 15)
+      * @param angle degrees to turn servo (-90 to +90)
+      */
+    //% blockId="setOffset"
+    //% block="set offset of servo %servo=e_servos| to %offset"
+    //% weight=80
+    //% subcategory=Servos
+    export function setOffset(servo: number, offset: number): void
+    {
+        servoOffset[servo] = offset;
+        setServo(servo, 0);
+    }
+
+    /**
+      * Return servo number from name
+      *
+      * @param value servo name
+      */
+    //% blockId="e_servos"
+    //% block="%value"
+    //% weight=70
+    //% subcategory=Servos
+    export function getServoNumber(value: eServos): number
+    {
+        return value;
+    }
 
 // MOTOR BLOCKS
 
@@ -174,61 +247,85 @@ namespace Rover
       * Drive forward (or backward) at speed.
       * @param speed speed of motor between -1023 and 1023. eg: 600
       */
-    //% blockId="cu_drive" block="drive at speed %speed"
+    //% blockId="drive"
+    //% block="drive at speed %speed"
     //% speed.min=-1023 speed.max=1023
-    //% weight=110
+    //% weight=100
+    //% subcategory=Motors
     export function drive(speed: number): void
     {
         motor(eMotor.Both, speed);
     }
 
+    /**
+      * Drive robot forward (or backward) at speed for milliseconds.
+      * @param speed speed of motor between -1023 and 1023. eg: 600
+      * @param milliseconds duration in milliseconds to drive forward for, then stop. eg: 400
+      */
+    //% blockId="drive_milliseconds"
+    //% block="drive at speed %speed| for %milliseconds|(ms)"
+    //% speed.min=-1023 speed.max=1023
+    //% weight=90
+    //% subcategory=Motors
+    export function driveMilliseconds(speed: number, milliseconds: number): void
+    {
+        drive(speed);
+        basic.pause(milliseconds);
+        stop(eStopMode.Coast);
+    }
+
+    /**
+      * Stop rover by coasting slowly to a halt or braking
+      * @param mode Brakes on or off
+      */
+    //% blockId="rover_stop" block="stop with %mode"
+    //% weight=80
+    //% subcategory=Motors
+    export function stop(mode: eStopMode): void
+    {
+        let stopMode = 0;
+        if (mode == eStopMode.Brake)
+            stopMode = 1;
+        pins.digitalWritePin(DigitalPin.P1, stopMode);
+        pins.digitalWritePin(DigitalPin.P12, stopMode);
+        pins.digitalWritePin(DigitalPin.P8, stopMode);
+        pins.digitalWritePin(DigitalPin.P0, stopMode);
+    }
 
     /**
       * Drive motor(s) forward or reverse.
       * @param motor motor to drive.
       * @param speed speed of motor eg: 600
       */
-    //% blockId="cu_motor" block="drive %motor| motor at speed %speed"
-    //% weight=100
+    //% blockId="motor"
+    //% block="drive %motor| motor at speed %speed"
+    //% weight=70
+    //% subcategory=Motors
     export function motor(motor: eMotor, speed: number): void
     {
-        let forward = (speed >= 0);
-        let absSpeed = Math.abs(speed);
-        if ((motor == eMotor.Left) || (motor == eMotor.Both))
-            leftSpeed = absSpeed;
-        if ((motor == eMotor.Right) || (motor == eMotor.Both))
-            rightSpeed = absSpeed;
-        setPWM();
-        if (speed > 1023)
+        let speed0 = 0;
+        let speed1 = 0;
+        setPWM(Math.abs(speed));
+        if (speed > 0)
         {
-            speed = 1023;
+            speed0 = speed;
+            speed1 = 0;
         }
-        else if (speed < -1023)
+        else
         {
-            speed = -1023;
-        }
-        let realSpeed = speed;
-        if (!forward)
-        {
-            if (realSpeed >= -200)
-                realSpeed = (realSpeed * 19) / 6;
-            else if (realSpeed >= -400)
-                realSpeed = realSpeed * 2;
-            else if (realSpeed >= -600)
-                realSpeed = (realSpeed * 3) / 2;
-            else if (realSpeed >= -800)
-                realSpeed = (realSpeed * 5) / 4;
-            realSpeed = 1023 + realSpeed; // realSpeed is negative
+            speed0 = 0;
+            speed1 = 0 - speed;
         }
         if ((motor == eMotor.Left) || (motor == eMotor.Both))
         {
-            pins.analogWritePin(AnalogPin.P8, realSpeed);
-            pins.digitalWritePin(DigitalPin.P0, forward ? 0 : 1);
+            pins.analogWritePin(AnalogPin.P1, speed0);
+            pins.analogWritePin(AnalogPin.P12, speed1);
         }
+
         if ((motor == eMotor.Right) || (motor == eMotor.Both))
         {
-            pins.analogWritePin(AnalogPin.P1, realSpeed);
-            pins.digitalWritePin(DigitalPin.P12, forward ? 0 : 1);
+            pins.analogWritePin(AnalogPin.P8, speed0);
+            pins.analogWritePin(AnalogPin.P0, speed1);
         }
     }
 
@@ -239,9 +336,11 @@ namespace Rover
     *
     * @param unit desired conversion unit
     */
-    //% blockId="cu_sonar" block="read sonar as %unit"
-    //% weight=90
-    export function sonar(unit: ePingUnit): number
+    //% blockId="readSonar"
+    //% block="read sonar as %unit"
+    //% weight=100
+    //% subcategory=Sensors
+    export function readSonar(unit: ePingUnit): number
     {
         // send pulse
         let trig = DigitalPin.P13;
@@ -277,35 +376,215 @@ namespace Rover
       * @param address Location in EEROM to write to
       * @param data Byte of data to write
       */
-    //% blockId="cu_writeEEROM" block="write %data| to address %address"
+    //% blockId="writeEEROM"
+    //% block="write %data| to address %address"
     //% data.min = -128 data.max = 127
+    //% weight=100
+    //% subcategory=EEROM
     export function writeEEROM(data: number, address: number): void
     {
-/*        let i2cData = pins.createBuffer(3);
+        /*let i2cData = pins.createBuffer(3);
 
         i2cData[0] = address >> 8;	// address MSB
         i2cData[1] = address & 0xff;	// address LSB
         i2cData[2] = data & 0xff;
         pins.i2cWriteBuffer(EEROM, i2cData, false);
-        servoOffset[address] = data;	// update servo offset as well - lazy coding
-        basic.pause(1);			// needs a short pause. << 1ms ok?*/
+        //servoOffset[address] = data;	// update servo offset as well - lazy coding
+        basic.pause(1);			// needs a short pause. << 1ms ok? */
     }
 
     /**
       * Read a byte of data from EEROM at selected address
       * @param address Location in EEROM to read from
       */
-    //% blockId="cu_readEEROM" block="read EEROM address %address"
+    //% blockId="readEEROM"
+    //% block="read EEROM address %address"
+    //% weight=90
+    //% subcategory=EEROM
     export function readEEROM(address: number): number
     {
-        return 0;	// no EEROM to read
-/*        let i2cRead = pins.createBuffer(2);
+        return 0;
+        /*let i2cRead = pins.createBuffer(2);
 
         i2cRead[0] = address >> 8;	// address MSB
         i2cRead[1] = address & 0xff;	// address LSB
         pins.i2cWriteBuffer(EEROM, i2cRead, false);
         basic.pause(1);
         return pins.i2cReadNumber(EEROM, NumberFormat.Int8LE);*/
+    }
+
+// LED Blocks
+
+    // create a neopixel strip if not got one already. Default to brightness 40
+    function neo(): neopixel.Strip
+    {
+        if (!neoStrip)
+        {
+            neoStrip = neopixel.create(DigitalPin.P2, 4, NeoPixelMode.RGB);
+            neoStrip.setBrightness(40);
+        }
+        return neoStrip;
+    }
+
+    // update LEDs if _updateMode set to Auto
+    function updateLEDs(): void
+    {
+        if (_updateMode == eUpdateMode.Auto)
+            neo().show();
+    }
+
+    /**
+      * Sets all LEDs to a given color (range 0-255 for r, g, b).
+      * @param rgb RGB color of the LED
+      */
+    //% blockId="set_led_color"
+    //% block="set all LEDs to %rgb=e_colours"
+    //% weight=100
+    //% subcategory=LEDs
+    export function setLedColor(rgb: number)
+    {
+        neo().showColor(rgb);
+        updateLEDs();
+    }
+
+    /**
+      * Clear all leds.
+      */
+    //% blockId="led_clear"
+    //% block="clear all LEDs"
+    //% weight=90
+    //% subcategory=LEDs
+    export function ledClear(): void
+    {
+        neo().clear();
+        updateLEDs();
+    }
+
+    /**
+     * Set single LED to a given color (range 0-255 for r, g, b).
+     *
+     * @param ledId position of the LED (0 to 11)
+     * @param rgb RGB color of the LED
+     */
+    //% blockId="set_pixel_color"
+    //% block="set LED at %ledId|to %rgb=e_colours"
+    //% weight=80
+    //% subcategory=LEDs
+    export function setPixelColor(ledId: number, rgb: number): void
+    {
+        neo().setPixelColor(ledId, rgb);
+        updateLEDs();
+    }
+
+    /**
+     * Set the brightness of the LEDs
+     * @param brightness a measure of LED brightness in 0-255. eg: 40
+     */
+    //% blockId="led_brightness"
+    //% block="set LED brightness %brightness"
+    //% brightness.min=0 brightness.max=255
+    //% weight=70
+    //% subcategory=LEDs
+    export function ledBrightness(brightness: number): void
+    {
+        neo().setBrightness(brightness);
+        updateLEDs();
+    }
+
+    /**
+      * Shows a rainbow pattern on all LEDs.
+      */
+    //% blockId="led_rainbow"
+    //% block="set led rainbow"
+    //% weight=60
+    //% subcategory=LEDs
+    export function ledRainbow(): void
+    {
+        neo().showRainbow(1, 360);
+        updateLEDs()
+    }
+
+    /**
+      * Get numeric value of colour
+      *
+      * @param color Standard RGB Led Colours
+      */
+    //% blockId="e_colours"
+    //% block=%color
+    //% weight=50
+    //% subcategory=LEDs
+    export function eColours(color: eColors): number
+    {
+        return color;
+    }
+
+// Advanced blocks
+
+    /**
+      * Set LED update mode (Manual or Automatic)
+      * @param updateMode setting automatic will show LED changes automatically
+      */
+    //% blockId="set_updateMode"
+    //% block="set %updateMode|update mode"
+    //% weight=100
+    //% advanced=true
+    export function setUpdateMode(updateMode: eUpdateMode): void
+    {
+        _updateMode = updateMode;
+    }
+
+    /**
+      * Show LED changes
+      */
+    //% blockId="led_show"
+    //% block="show LED changes"
+    //% weight=90
+    //% advanced=true
+    export function ledShow(): void
+    {
+        neo().show();
+    }
+
+    /**
+     * Rotate LEDs forward.
+     */
+    //% blockId="led_rotate"
+    //% block="rotate LEDs"
+    //% weight=80
+    //% advanced=true
+    export function ledRotate(): void
+    {
+        neo().rotate(1);
+        updateLEDs()
+    }
+
+    /**
+     * Shift LEDs forward and clear with zeros.
+     */
+    //% blockId="led_shift"
+    //% block="shift LEDs"
+    //% weight=70
+    //% advanced=true
+    export function ledShift(): void
+    {
+        neo().shift(1);
+        updateLEDs()
+    }
+
+    /**
+      * Convert from RGB values to colour number
+      *
+      * @param red Red value of the LED (0 to 255)
+      * @param green Green value of the LED (0 to 255)
+      * @param blue Blue value of the LED (0 to 255)
+      */
+    //% blockId="convertRGB"
+    //% block="convert from red %red| green %green| blue %blue"
+    //% weight=60
+    //% advanced=true
+    export function convertRGB(r: number, g: number, b: number): number
+    {
+        return ((r & 0xFF) << 16) | ((g & 0xFF) << 8) | (b & 0xFF);
     }
 
 }
